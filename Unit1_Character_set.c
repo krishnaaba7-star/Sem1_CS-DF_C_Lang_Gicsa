48dd04ad57af17d0c1254c24666dc44f1792e27b
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-21 10:18:54 +0530

    Unit1 : Character set

---
+++ Unit1_Character_set.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    char name;
+    int number = 100;
+    printf("Hello World!");
+    printf("Hello Cyber\n");
+    printf("Hello Students\t");
+
+    return 0;
+}
