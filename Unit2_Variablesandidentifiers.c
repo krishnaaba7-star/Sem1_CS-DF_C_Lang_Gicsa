0ef985357d2385509653508a7e26dbeec1247c86
krishnaaba7-star <krishnaaba7@gmail.com>
2026-02-25 19:33:58 +0530

    Unit1_Variablesandidentifiers.c

--- Unit2_Variablesandidentifiers.c
+++ Unit2_Variablesandidentifiers.c
@@ -5,6 +5,19 @@ int main() {
    int age = 20;
     float price = 99.5;
     char grade = 'A';
+    int number;
+
+//    int 1age;      // starts with number
+// int total$;    // special character
+// int float;     // keyword
+
+   int age;
+   int _total;
+   int marks1;
+
+   int age = 25;   // age is a variable and identifier
+   printf("Age is %d", age);
+   return 0;
 
     return 0;
 }
