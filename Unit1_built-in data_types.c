5f637ea613a9c4331ad7e3f2d9ad084a3e82946c
krishnaaba7-star <krishnaaba7@gmail.com>
2026-03-01 19:36:21 +0530

    Unit1_built-in data_types.c

---
+++ Unit1_built-in data_types.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float price = 99.99;
+    double pi = 3.1415926535;
+    char grade = 'A';
+    short int x;
+    long int y;
+    long long int z;
+    unsigned int num = 50;
+    void display() {
+    printf("Hello");
+}
+
+
+    int a = 10;
+    float b = 5.5;
+    char c = 'Z';
+    double d = 3.14159;
+
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Character: %c\n", c);
+    printf("Double: %lf\n", d);
+    return 0;
+}
